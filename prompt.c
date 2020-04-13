898f9156de621eeedca9eb20de3b84c5dfebd73d
Bzhamdi <bouzouitina.hamdi@gmail.com>
2020-04-13 00:43:30 +0000

    prompt

---
+++ prompt.c
@@ -0,0 +1,25 @@
+#include "shell.h"
+
+/**
+ * print_prompt - verif if is terminal.
+ *@test: int
+ */
+
+void print_prompt(int test)
+{
+	if (!isatty(STDIN_FILENO))
+		test = 1;
+	if (test == 0)
+		_puts("#BZ$ ");
+}
+
+/**
+ * sig_handler - checks Ctrl + C is pressed.
+ * @sig: int.
+ */
+void sig_handler(int sig)
+{
+	if (sig == SIGINT)
+		_puts("\n#BZ$ ");
+        
+}
